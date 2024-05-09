c4f76525c17e555b688db11dd3ae1b3d985e3511
Harshitk816 <harshitkaushal816@gmail.com>
2024-05-09 21:38:05 +0530

    cors error fixed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,9 +3,9 @@ export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_los
 
 export const LOGO_URL="https://pbs.twimg.com/media/GD-K1rYbAAAoOEs?format=png&name=900x900"
 
-export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+export const MENU_API="https://cors-handlers.vercel.app/api/?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D12.9351929%26lng%3D77.62448069999999%26restaurantId%3D";
 
-export const SWIGGY_API="https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const SWIGGY_API="https://cors-handlers.vercel.app/api/?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D12.9351929%26lng%3D77.62448069999999%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
 
 export const SEARCH_BG = "https://pbs.twimg.com/media/GEmqhKWagAAV-gH?format=jpg&name=4096x4096"
 
